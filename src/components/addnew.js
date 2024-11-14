88c6d349137e629ce80c0fcb20d183559df0cebd
Jia0705 <160045168+Jia0705@users.noreply.github.com>
2024-11-14 15:37:33 +0800

    Update addnew.js

--- src/components/addnew.js
+++ src/components/addnew.js
@@ -1,44 +1,39 @@
-import { useState } from "react";
+import{useState} from 'react';
 
 function AddNewForm(props) {
-  const { onNewTaskAdded } = props;
-  const [taskName, setTaskName] = useState("");
+
+  const {onNewTaskAdded} = props
+  const [taskName,setTaskText] = useState("");
+
   return (
-    <div className="mt-4">
-      <form className="d-flex justify-content-between align-items-center">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Add new item..."
-          name="task_name"
-          value={taskName}
-          required
-          onChange={(event) => {
-            // update the taskName state
-            setTaskName(event.target.value);
-          }}
-        />
-        <button
-          className="btn btn-primary btn-sm rounded ms-2"
-          onClick={(event) => {
-            // disable the button submit behaviour
-            event.preventDefault();
-            // check for empty string
-            if (taskName === "") {
-              alert("Please enter the task");
-            } else {
-              // passing the name back to the parent component
+    <div>
+      <form>
+        <div className="mt-4 d-flex justify-content-between align-items-center">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Add new item..."
+            name="taskText"
+            value={taskName}
+            required
+            onChange={(event) => setTaskText(event.target.value)}
+          />
+          <button className="btn btn-primary btn-sm rounded ms-2" onClick={(event) => {event.preventDefault();
+            if(taskName === "") {
+              alert("Please enter the text");
+            }else {
               onNewTaskAdded(taskName);
-              // reset the state
-              setTaskName("");
+              setTaskText("");
             }
-          }}
-        >
-          Add
-        </button>
+          }}>Add</button>
+          
+        </div>
       </form>
     </div>
   );
-}
-
-export default AddNewForm;
+  }
+   
+  
+  
+  export default AddNewForm;
+  
