b57dd308b7e84e1886d7017f3082f0dc589b6b48
Jia0705 <160045168+Jia0705@users.noreply.github.com>
2024-11-14 15:37:16 +0800

    Update item.js

--- src/components/item.js
+++ src/components/item.js
@@ -1,43 +1,28 @@
-function Item(props) {
-  const { id, text, isCompleted, onTaskDelete, onTaskDone } = props;
-  return (
-    <li className="list-group-item d-flex justify-content-between align-items-center">
-      <div>
-        <button
-          className={`btn btn-sm ${isCompleted ? "btn-success" : "btn-light"}`}
-          onClick={() => onTaskDone(id)}
-        >
-          <i
-            className={`bi ${isCompleted ? "bi-check-square" : "bi-square"}`}
-            onClick={() => onTaskDone(id)}
-          ></i>
+import React from 'react';
+
+
+function Item({task, toggleTask,deleteTask}) {
+    const {id , text, isCompleted }= task;
+    return (
+      <li className="list-group-item d-flex justify-content-between align-items-center">
+        <div>
+        <button className={`btn btn-sm ${isCompleted ? 'btn-success' : 'btn-light'}`} onClick={() => toggleTask(id)}>
+        <i className={`bi ${isCompleted ? 'bi-check-square' : 'bi-square'}`}></i>
         </button>
-        <span
-          className={`ms-2 ${
-            isCompleted ? "text-decoration-line-through" : ""
-          }`} onClick={() => onTaskDone(id)}
-        >
-          {text}
-        </span>
-      </div>
-      <div>
-        <button
-          className="btn btn-sm btn-danger"
-          onClick={() => {
-            const confirm = window.confirm(
-              "Are you sure you want to delete this task?"
-            );
-            if (confirm) {
-              // pass the id up to the List
-              onTaskDelete(id);
-            }
-          }}
-        >
-          <i className="bi bi-trash"></i>
-        </button>
-      </div>
-    </li>
-  );
+        <span className={`ms-2 ${isCompleted ? 'text-decoration-line-through' : ''}`}>
+        {text}</span>
+        </div>
+        <button className="btn btn-sm btn-danger" onClick={() => {const confirm =window.confirm("Are you sure want to delete this task");
+          if (confirm) {
+            deleteTask(id)
+          }
+          }
+        }
+         >
+                <i class="bi bi-trash"></i>
+          </button>
+      </li>
+    );
 }
 
 export default Item;
