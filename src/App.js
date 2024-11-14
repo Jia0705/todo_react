c69ee4161347160f2ef414f125227f45484db5d3
Jia0705 <160045168+Jia0705@users.noreply.github.com>
2024-11-14 15:38:06 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import { useState } from "react";
-import { nanoid } from "nanoid";
-
+import React, {useState} from 'react';
+import {nanoid} from 'nanoid'
 import AddNewForm from "./components/addnew";
-import ItemsList from "./components/list";
+import ItemList from "./components/list";
 
 function App() {
-  const [list, setList] = useState([]);
+
+  const [list, setList] = useState ([])
+
 
   return (
+    <div className="App">Todo
     <div
       className="card rounded shadow-sm"
-      style={{ maxWidth: "500px", margin: "60px auto" }}
+      style={{
+        maxWidth: "500px",
+        margin: "60px auto"
+      }}
     >
       <div className="card-body">
         <h3 className="card-title mb-3">My Todo List</h3>
-        <ItemsList
+        <ItemList
           list={list}
-          onTaskDelete={(id) => {
+          deleteTask={(id) => {
             // filter OUT the task with the given id
             const newList = list.filter((t) => t.id !== id);
             // update the newList with the setState function
             setList(newList);
           }}
           
-          onTaskDone={(id) => {
-            const sleepy = list.map((item) => {
+          toggleTask={(id) => {
+            const taskList = list.map((item) => {
               if (item.id === id) {
                 return {...item, isCompleted: !item.isCompleted};
               } else{
               return item;
               }
             });
-            setList(sleepy);
+            setList(taskList);
           }}
         />
-
+        
         <AddNewForm
           onNewTaskAdded={(taskName) => {
             // clone the existing state
@@ -49,9 +54,11 @@ function App() {
             setList(newList);
           }}
         />
+        </div>      
       </div>
-    </div>
+     </div>
   );
 }
+
 
 export default App;
